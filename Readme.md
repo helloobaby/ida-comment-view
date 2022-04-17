0a217155ca4b73b6cbc476685f0f89159b495fdb
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2022-04-17 23:56:42 +0800

    update

--- Readme.md
+++ Readme.md
@@ -4,10 +4,15 @@
 
 
 Example:  
-cmake -DIDA_INSTALL_DIR="E:\IDA Pro 7.6" ..
+mkdir build && cd build && cmake -DIDA_INSTALL_DIR="E:\IDA Pro 7.6" ..
 
 
 ida32和ida64都是64位的,但是分析的程序有32和64,如果分析32的话,需要改一下cmakelist,把x64_win_vc_64改成x64_win_vc_32
 
 
 编译完之后ida打开会自动加载插件测试(上面指定了ida安装目录)
+
+
+
+ida 7.5前到7.5后 api迁移记录  
+https://www.hex-rays.com/products/ida/support/ida74_idapython_no_bc695_porting_guide.shtml
--- src/pluginmain.cpp
+++ src/pluginmain.cpp
@@ -10,89 +10,71 @@
 #define NO_OBSOLETE_FUNCS
 #include <hexrays_sdk/include/hexrays.hpp>
 
-//IDA内部用的名字，识别每个UI部件的,前面加作用域是IDA的推荐做法
+// IDA内部用的名字，识别每个UI部件的,前面加作用域是IDA的推荐做法
 #define action_internal_name_1 "example::name"
 
 //这个是右键显示给用户的名字
 #define action_show_name_1 "example"
 
-
-
-ssize_t idaapi ui_hook(void* user_data, int notification_code, va_list va)
-{
-    if (notification_code == ui_populating_widget_popup)
-    {
-        TWidget* view = va_arg(va, TWidget*);
-        if (get_widget_type(view) == BWN_DISASM) {
-            TPopupMenu* p = va_arg(va, TPopupMenu*);
-            attach_action_to_popup(view, p, action_internal_name_1, nullptr, SETMENU_FIRST);
-        }
+ssize_t idaapi ui_hook(void* user_data, int notification_code, va_list va) {
+  if (notification_code == ui_populating_widget_popup) {
+    TWidget* view = va_arg(va, TWidget*);
+    if (get_widget_type(view) == BWN_DISASM) {
+      TPopupMenu* p = va_arg(va, TPopupMenu*);
+      attach_action_to_popup(view, p, action_internal_name_1, nullptr,
+                             SETMENU_FIRST);
     }
+  }
 
-    return false;
+  return false;
 }
 
-struct example_action : public action_handler_t
-{
-    //action被触发的主逻辑
-    virtual int idaapi activate(action_activation_ctx_t* ctx) override
-    {
-        msg("hello world");
-        return true;
-    }
+struct example_action : public action_handler_t {
+  // action被触发的主逻辑
+  virtual int idaapi activate(action_activation_ctx_t* ctx) override {
+    msg("hello world");
+    return true;
+  }
 
-    virtual action_state_t idaapi update(action_update_ctx_t* ctx) override
-    {
-        //AST_DISABLE选项的话action就是灰色，无法点
-        return AST_ENABLE_ALWAYS;
-    }
+  virtual action_state_t idaapi update(action_update_ctx_t* ctx) override {
+    // AST_DISABLE选项的话action就是灰色，无法点
+    return AST_ENABLE_ALWAYS;
+  }
 };
 
 example_action action1;
-struct plugin_ctx_t : public plugmod_t
-{
-    plugin_ctx_t()
-    {
-        register_action(ACTION_DESC_LITERAL_PLUGMOD(
-            action_internal_name_1,// action name
-            action_show_name_1, // show name
-            &action1,
-            this,
-            nullptr,
-            nullptr,
-            -1));
+struct plugin_ctx_t : public plugmod_t {
+  plugin_ctx_t() {
+    register_action(
+        ACTION_DESC_LITERAL_PLUGMOD(action_internal_name_1,  // action name
+                                    action_show_name_1,      // show name
+                                    &action1, this, nullptr, nullptr, -1));
 
-        hook_to_notification_point(HT_UI, ui_hook);
-    }
-    ~plugin_ctx_t()
-    {
-        unregister_action(action_internal_name_1);
-        unhook_from_notification_point(HT_UI, ui_hook);
-        term_hexrays_plugin();
-    }
-    virtual bool idaapi run(size_t) override;
+    hook_to_notification_point(HT_UI, ui_hook);
+  }
+  ~plugin_ctx_t() {
+    unregister_action(action_internal_name_1);
+    unhook_from_notification_point(HT_UI, ui_hook);
+    term_hexrays_plugin();
+  }
+  virtual bool idaapi run(size_t) override;
 };
 
-
-
 //--------------------------------------------------------------------------
-bool idaapi plugin_ctx_t::run(size_t)
-{
-    msg("hello world");
-    
-    return true;
+bool idaapi plugin_ctx_t::run(size_t) {
+  msg("hello world");
+
+  return true;
 }
 
 //--------------------------------------------------------------------------
 // Initialize the plugin.
-static plugmod_t* idaapi init()
-{
-    if (!init_hexrays_plugin())
-        return nullptr; // no decompiler
-    const char* hxver = get_hexrays_version();
-    msg("Hex-rays version %s has been detected, %s ready to use\n",
-        hxver, PLUGIN.wanted_name);
-    return new plugin_ctx_t;
+static plugmod_t* idaapi init() {
+  if (!init_hexrays_plugin()) return nullptr;  // no decompiler
+  const char* hxver = get_hexrays_version();
+  msg("Hex-rays version %s has been detected, %s ready to use\n", hxver,
+      PLUGIN.wanted_name);
+  return new plugin_ctx_t;
 }
 
 //--------------------------------------------------------------------------
@@ -103,15 +85,14 @@ static const char comment[] = "whatever you want";
 //      PLUGIN DESCRIPTION BLOCK
 //
 //--------------------------------------------------------------------------
-plugin_t PLUGIN =
-{
-  IDP_INTERFACE_VERSION,
-  PLUGIN_MULTI|PLUGIN_UNL,//PLUGIN_UNL用来调试，run一次就自动卸载的功能
-  init,                 // initialize
-  nullptr,
-  nullptr,
-  comment,              // long comment about the plugin
-  nullptr,              // multiline help about the plugin
-  "whatever you want", // 在Edit->Plugins中显示的名字
-  nullptr,              // 热键
+plugin_t PLUGIN = {
+    IDP_INTERFACE_VERSION,
+    PLUGIN_MULTI | PLUGIN_UNL,  // PLUGIN_UNL用来调试，run一次就自动卸载的功能
+    init,                       // initialize
+    nullptr,
+    nullptr,
+    comment,              // long comment about the plugin
+    nullptr,              // multiline help about the plugin
+    "whatever you want",  // 在Edit->Plugins中显示的名字
+    nullptr,              // 热键
 };
